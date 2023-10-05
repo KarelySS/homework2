26eba307c38c5e8a5682df6cb4e7a736019976e8
79535 <twinm124@gmail.com>
2023-10-05 20:41:58 +0300

    Frog

--- src/Main.java
+++ src/Main.java
@@ -2,32 +2,42 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         var dog=8.0;
-        System.out.println("Собака"+ dog);
+        System.out.println("Собака "+ dog);
         var cat=3.6;
-        System.out.println("Кошка"+ cat);
+        System.out.println("Кошка "+ cat);
         var paper=763789;
-        System.out.println("Бумага"+ paper);
+        System.out.println("Бумага "+ paper);
 
         dog=dog+4;
-        System.out.println("Собака"+ dog);
+        System.out.println("Собака "+ dog);
         cat=cat+4;
-        System.out.println("Кошка"+ cat);
+        System.out.println("Кошка "+ cat);
         paper=paper+4;
-        System.out.println("Бумага"+ paper);
+        System.out.println("Бумага "+ paper);
 
         var resault1=dog-3.5;
-        System.out.println("Собака"+ resault1);
+        System.out.println("Собака "+ resault1);
         var resault2=cat-1.6;
-        System.out.println("Кошка"+ resault2);
+        System.out.println("Кошка "+ resault2);
         var resault3=paper-7639;
-        System.out.println("Бумага"+ resault3);
+        System.out.println("Бумага "+ resault3);
 
         var friend=19;
-        System.out.println("Друг"+ friend);
-        friend=friend*2;
-        System.out.println("Друг"+ friend);
+        System.out.println("Друг "+ friend);
+        friend=friend+2;
+        System.out.println("Друг "+ friend);
         friend=friend/7;
-        System.out.println("Друг"+ friend);
+        System.out.println("Друг "+ friend);
+
+        var frog=3.5;
+        System.out.println("Лягушка "+ frog);
+        frog=frog*10;
+        System.out.println("Лягушка "+ frog);
+        frog=frog/3.5;
+        System.out.println("Лягушка "+ frog);
+        frog=frog+4;
+        System.out.println("Лягушка "+ frog);
+
 
 
 
